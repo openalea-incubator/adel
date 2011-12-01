215c516f0e7bd81100dd3d31edc05ae27308c13a
Camille Chambon <cchambon@grignon.inra.fr>
2011-12-01 16:51:42 +0000

    Add VisuAlea "Data file"s  to use thermal_time_results.csv from VisuAlea.

--- adel/fit/__wralea__.py
+++ adel/fit/__wralea__.py
@@ -1,5 +1,5 @@
 
-# This file has been generated at Mon Nov 07 12:25:28 2011
+# This file has been generated at Wed Nov 30 16:16:08 2011
 
 from openalea.core import *
 
@@ -17,7 +17,22 @@ __institutes__ = 'INRA, CIRAD, INRIA'
 __icon__ = ''
 
 
-__all__ = ['fit_fit_leaves', 'fit_fit_leaf', 'fit_fit', 'dimension_fitting_dimension_fitting', 'fit_simplify']
+__all__ = ['thermal_time_thermal_time', 'fit_fit_leaves', 'fit_simplify', 'fit_fit_leaf', 'dimension_fitting_dimension_fitting', 'fit_fit']
+
+
+
+thermal_time_thermal_time = Factory(name='thermal_time',
+                authors='C. Pradal, C. Fournier (wralea authors)',
+                description='Calculates the thermal time increment.',
+                category='data processing',
+                nodemodule='thermal_time',
+                nodeclass='thermal_time',
+                inputs=[{'interface': None, 'name': 'requested_dates', 'value': None, 'desc': 'The dates for which the thermal time increment has to be calculated. These dates must belong to input_data, and be >= to emergence_date.'}, {'interface': IDict, 'name': 'emergence_date', 'value': {'hour': 0, 'month': 5, 'second': 0, 'year': 1998, 'day': 31, 'minute': 0}, 'desc': 'The emergence date. The interval [emergence date, emergence date + 1 year[ must be included in input_data. emergence_datetime must be <= than requested_dates'}, {'interface': None, 'name': 'input_data', 'value': None, 'desc': 'The input data. Expect a pandas.DataFrame object. The dataframe index must contain dates.'}, {'interface': IEnumStr(enum=['daily', 'hourly']), 'name': 'data_type', 'value': None, 'desc': "The type of the input data. Can be either 'daily' or 'hourly'."}, {'interface': IEnumStr(enum=['dsT', 'dsTc']), 'name': 'thermal_time_increment_method', 'value': None, 'desc': "The method used for thermal time increment calculation. Can be either 'dsT' or 'dsTc' (compensated)."}, {'interface': IFloat, 'name': 'latitude', 'value': 55, 'desc': "The latitude where the data have been measured. Used to convert 'daily' data to 'hourly' data."}],
+                outputs=[{'interface': None, 'name': 'temperature_increment', 'desc': 'The thermal time increment calculated (pandas.DataFrame).'}],
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
 
 
 
@@ -29,6 +44,21 @@ fit_fit_leaves = Factory(name='fit leaves',
                 nodeclass='fit_leaves',
                 inputs=[{'interface': IDict, 'name': 'leaves', 'value': {}, 'desc': ''}, {'interface': IInt, 'name': 'nb_points', 'value': 7, 'desc': ''}],
                 outputs=[{'interface': IDict, 'name': 'leaves', 'desc': 'New database'}],
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+fit_simplify = Factory(name='simplification',
+                authors='C. Pradal, C. Fournier (wralea authors)',
+                description='leaf simplification',
+                category='fitting',
+                nodemodule='fit',
+                nodeclass='simplify',
+                inputs=[{'interface': ISequence, 'name': 'leaf', 'value': [], 'desc': 'midrib (x, y, s, r)'}, {'interface': IInt, 'name': 'nb_points', 'value': 3, 'desc': 'target number of points after simplification'}],
+                outputs=[{'interface': ISequence, 'name': 'leaf'}],
                 widgetmodule=None,
                 widgetclass=None,
                )
@@ -51,21 +81,6 @@ fit_fit_leaf = Factory(name='fit midrib',
 
 
 
-fit_fit = Factory(name='fit',
-                authors='C. Pradal, C. Fournier (wralea authors)',
-                description='fit a leaf (x, y, s, r)',
-                category='fitting',
-                nodemodule='fit',
-                nodeclass='fit',
-                inputs=[{'interface': ISequence, 'name': 'x', 'value': [], 'desc': 'midrib axis'}, {'interface': ISequence, 'name': 'y', 'value': [], 'desc': 'midrib ordinate'}, {'interface': ISequence, 'name': 's', 'value': [], 'desc': 'midrib curvilinear abscisse'}, {'interface': ISequence, 'name': 'r', 'value': [], 'desc': 'midrib radius'}, {'interface': IInt, 'name': 'nb_points', 'value': 3, 'desc': 'target number of points after simplification'}],
-                outputs=[{'interface': ISequence, 'name': 'leaf'}],
-                widgetmodule=None,
-                widgetclass=None,
-               )
-
-
-
-
 dimension_fitting_dimension_fitting = Factory(name='dimension_fitting',
                 authors='C. Pradal, C. Fournier (wralea authors)',
                 description='',
@@ -81,13 +96,13 @@ dimension_fitting_dimension_fitting = Factory(name='dimension_fitting',
 
 
 
-fit_simplify = Factory(name='simplification',
+fit_fit = Factory(name='fit',
                 authors='C. Pradal, C. Fournier (wralea authors)',
-                description='leaf simplification',
+                description='fit a leaf (x, y, s, r)',
                 category='fitting',
                 nodemodule='fit',
-                nodeclass='simplify',
-                inputs=[{'interface': ISequence, 'name': 'leaf', 'value': [], 'desc': 'midrib (x, y, s, r)'}, {'interface': IInt, 'name': 'nb_points', 'value': 3, 'desc': 'target number of points after simplification'}],
+                nodeclass='fit',
+                inputs=[{'interface': ISequence, 'name': 'x', 'value': [], 'desc': 'midrib axis'}, {'interface': ISequence, 'name': 'y', 'value': [], 'desc': 'midrib ordinate'}, {'interface': ISequence, 'name': 's', 'value': [], 'desc': 'midrib curvilinear abscisse'}, {'interface': ISequence, 'name': 'r', 'value': [], 'desc': 'midrib radius'}, {'interface': IInt, 'name': 'nb_points', 'value': 3, 'desc': 'target number of points after simplification'}],
                 outputs=[{'interface': ISequence, 'name': 'leaf'}],
                 widgetmodule=None,
                 widgetclass=None,

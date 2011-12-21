16a527e2605534b7425cc5190374583725963fda
Adrian Koller <adrian.koller@okstate.edu>
2011-12-21 15:09:20 +0000

    modified adel maize into maizeAzim.lpy that allows control of azimuth of plants

--- adel/data/__wralea__.py
+++ adel/data/__wralea__.py
@@ -1,5 +1,5 @@
 
-# This file has been generated at Wed Dec 21 10:38:45 2011
+# This file has been generated at Wed Dec 21 15:48:46 2011
 
 from openalea.core import *
 
@@ -17,305 +17,312 @@ __institutes__ = 'INRA'
 __icon__ = ''
 
 
-__all__ = ['_73289040', '_73288432', '_73289232', '_73289488', '_73289168', '_73288944', '_73288656', '_73288752', '_73289648', '_73289520', '_73289424', '_73288496', '_73288688', '_73288848', '_73288464', '_73289328', '_73288880', '_73289072', '_73289136', '_73289360', '_73289264', '_73288400', '_73289712', '_73288784', '_120954800', '_73289552', '_73289616', '_73289200', '_73289456', '_73288976', '_73288560', '_73288912', '_73288816', '_73288528', '_73288624', '_73288720', '_73289296', '_73289392', '_73289104', '_73289680', '_73289008', '_73288592', '_73289584']
+__all__ = ['_91833424', '_91832592', '_91832656', '_91833680', '_91833456', '_91832848', '_91833520', '_91832752', '_91833488', '_91833136', '_91833008', '_91833232', '_91833040', '_91833648', '_91833584', '_91833744', '_91833840', '_91833392', '_91833296', '_91833808', '_129805776', '_91832976', '_91832720', '_91833552', '_91832816', '_91832912', '_91833360', '_91832624', '_91833104', '_91833200', '_91833072', '_91833264', '_91833616', '_91833872', '_91832784', '_91833712', '_91833776', '_91832688', '_91833168', '_91832560', '_91832528', '_91833328', '_91832880', '_91832944']
 
 
-_73289040 = DataFactory(name='earTCa0N.csv',
-                    description='sample parameterisation for ear growth',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288432 = DataFactory(name='dimTSoissons.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289232 = DataFactory(name='scene.8',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289488 = DataFactory(name='Jess_earTCa0N.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289168 = DataFactory(name='Arvalis.RData',
-                    description='Calibrated Wheat architecture database from Avalis experiment on septoriose',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288944 = DataFactory(name='senleaf.png',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288656 = DataFactory(name='leaves.db',
-                    description='Database of leaves of a maize genotype.',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288752 = DataFactory(name='RMh.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289648 = DataFactory(name='phenTNormalPhyllochron.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289520 = DataFactory(name='requested_dates.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289424 = DataFactory(name='greenleaf.png',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288496 = DataFactory(name='metAdel03-04.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288688 = DataFactory(name='phenTIncreased20Phyllochron.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288848 = DataFactory(name='axeTCa0N.csv',
-                    description='Demo cvs file for axeT input',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288464 = DataFactory(name='phenTCa0N.csv',
-                    description='demo csv file for phenT input',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289328 = DataFactory(name='Inputdim_SoNp_to_fit.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288880 = DataFactory(name='metAdel00-01.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289072 = DataFactory(name='dimTCactus.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289136 = DataFactory(name='dimTOiseaux.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289360 = DataFactory(name='mais.RData',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289264 = DataFactory(name='ssi2sen.csv',
-                    description='specification of leaf senescence patterns for upper leaves as a function of ssi',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288400 = DataFactory(name='RMj.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289712 = DataFactory(name='SRTino.RData',
-                    description='SR profile by TDornbush et al , Ann Bot 2010',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73288784 = DataFactory(name='Jess_dimT_SoNp_bis.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_120954800 = DataFactory(name='Adel2.lpy',
-                    description='lpy version of adel maize',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289552 = DataFactory(name='Jess_ssi2sen.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289616 = DataFactory(name='metAdel08-09.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289200 = DataFactory(name='Jess_phenT_SoNp.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289456 = DataFactory(name='Rfun.R',
+_91833424 = DataFactory(name='Rfun.R',
                     description='Sample file of R user functions to control Adel',
                     editors=None,
                     includes=None,
                     )
 
 
-_73288976 = DataFactory(name='docAdel.R',
+_91832592 = DataFactory(name='scene.8',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832656 = DataFactory(name='Arvalis.RData',
+                    description='Calibrated Wheat architecture database from Avalis experiment on septoriose',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833680 = DataFactory(name='axeTSoissonsNormal_10plants.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833456 = DataFactory(name='docAdel.R',
                     description='Small doc for Adel inputs',
                     editors=None,
                     includes=None,
                     )
 
 
-_73288560 = DataFactory(name='turtle46.light',
+_91832848 = DataFactory(name='greenleaf.png',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833520 = DataFactory(name='So09.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832752 = DataFactory(name='RMh.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833488 = DataFactory(name='turtle46.light',
                     description='Turtle with 46 sectors',
                     editors=None,
                     includes=None,
                     )
 
 
-_73288912 = DataFactory(name='So09.RData',
+_91833136 = DataFactory(name='mais.RData',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_73288816 = DataFactory(name='turtle6.light',
+_91833008 = DataFactory(name='Inputdim_SoNp_to_fit.csv',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_73288528 = DataFactory(name='simpleMais.csv',
+_91833232 = DataFactory(name='SRTino.RData',
+                    description='SR profile by TDornbush et al , Ann Bot 2010',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833040 = DataFactory(name='metAdel00-01.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833648 = DataFactory(name='thermal_time_results.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833584 = DataFactory(name='simpleMais.csv',
                     description='Simple parameterisation of mais for TD',
                     editors=None,
                     includes=None,
                     )
 
 
-_73288624 = DataFactory(name='HMj.csv',
+_91833744 = DataFactory(name='Jess_axeT_SoNp.csv',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_73288720 = DataFactory(name='thermal_time_results.csv',
+_91833840 = DataFactory(name='maize.lpy',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_73289296 = DataFactory(name='axeTSoissonsNormal_10plants.csv',
+_91833392 = DataFactory(name='Jess_phenT_SoNp.csv',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_73289392 = DataFactory(name='leaves_simple.db',
+_91833296 = DataFactory(name='Adel2.lpy',
+                    description='lpy version of adel maize',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833808 = DataFactory(name='axeTSoissonsUniculm_30plants.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_129805776 = DataFactory(name='maizeAzim.lpy',
+                    description='adel maize with azimuth',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832976 = DataFactory(name='phenTCa0N.csv',
+                    description='demo csv file for phenT input',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832720 = DataFactory(name='leaves.db',
+                    description='Database of leaves of a maize genotype.',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833552 = DataFactory(name='turtle6.light',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832816 = DataFactory(name='requested_dates.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832912 = DataFactory(name='phenTIncreased20Phyllochron.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833360 = DataFactory(name='metAdel08-09.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832624 = DataFactory(name='Jess_earTCa0N.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833104 = DataFactory(name='dimTOiseaux.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833200 = DataFactory(name='RMj.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833072 = DataFactory(name='dimTCactus.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833264 = DataFactory(name='Jess_dimT_SoNp_bis.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833616 = DataFactory(name='HMj.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833872 = DataFactory(name='turtle16.light',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832784 = DataFactory(name='phenTNormalPhyllochron.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833712 = DataFactory(name='leaves_simple.db',
                     description='One maize leaf per order',
                     editors=None,
                     includes=None,
                     )
 
 
-_73289104 = DataFactory(name='Jess_axeT_SoNp.csv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_73289680 = DataFactory(name='dimTCa0N.csv',
+_91833776 = DataFactory(name='dimTCa0N.csv',
                     description='Demo csv file for dimT',
                     editors=None,
                     includes=None,
                     )
 
 
-_73289008 = DataFactory(name='axeTSoissonsUniculm_30plants.csv',
+_91832688 = DataFactory(name='senleaf.png',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_73288592 = DataFactory(name='maize.lpy',
+_91833168 = DataFactory(name='ssi2sen.csv',
+                    description='specification of leaf senescence patterns for upper leaves as a function of ssi',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832560 = DataFactory(name='dimTSoissons.csv',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_73289584 = DataFactory(name='turtle16.light',
+_91832528 = DataFactory(name='earTCa0N.csv',
+                    description='sample parameterisation for ear growth',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91833328 = DataFactory(name='Jess_ssi2sen.csv',
                     description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832880 = DataFactory(name='metAdel03-04.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_91832944 = DataFactory(name='axeTCa0N.csv',
+                    description='Demo cvs file for axeT input',
                     editors=None,
                     includes=None,
                     )

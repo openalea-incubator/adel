00da3e2a84b159912af0fff33cf68898e824d3a1
Camille Chambon <cchambon@grignon.inra.fr>
2012-02-27 15:42:18 +0000

    Move the test data files from test/data/ to test/data/test_fitting2/ for more clarity. Adapt test_fitting2.py accordingly.

--- test/test_fitting2.py
+++ test/test_fitting2.py
@@ -18,25 +18,25 @@ fitting_results_directory = path(tempfile.mkdtemp(suffix='_fitting_results'))
 
 def setup_module():
 
-    config.read(path('data/default_input_paramters.cfg'))
+    config.read(path('data/test_fitting2/default_input_paramters.cfg'))
     
-    expected_first_axis_table_dataframe = pandas.read_csv(path('data/first_axis_table.csv'))
-    expected_first_parameters_dataframe = pandas.read_csv(path('data/first_parameters.csv'))  
+    expected_first_axis_table_dataframe = pandas.read_csv(path('data/test_fitting2/first_axis_table.csv'))
+    expected_first_parameters_dataframe = pandas.read_csv(path('data/test_fitting2/first_parameters.csv'))  
     expected_fit_adel_input_data_first_dict.update(axis_table=expected_first_axis_table_dataframe, 
                                                    parameters=expected_first_parameters_dataframe)
-    linear_expected_second_axis_table_dataframe = pandas.read_csv(path('data/linear_second_axis_table.csv'))
-    linear_expected_absolute_phen_table_dataframe = pandas.read_csv(path('data/linear_second_absolute_phen_table.csv'))
-    linear_expected_relative_phen_table_dataframe = pandas.read_csv(path('data/linear_second_relative_phen_table.csv'))
-    linear_expected_second_parameters_dataframe = pandas.read_csv(path('data/linear_second_parameters.csv'))
+    linear_expected_second_axis_table_dataframe = pandas.read_csv(path('data/test_fitting2/linear_second_axis_table.csv'))
+    linear_expected_absolute_phen_table_dataframe = pandas.read_csv(path('data/test_fitting2/linear_second_absolute_phen_table.csv'))
+    linear_expected_relative_phen_table_dataframe = pandas.read_csv(path('data/test_fitting2/linear_second_relative_phen_table.csv'))
+    linear_expected_second_parameters_dataframe = pandas.read_csv(path('data/test_fitting2/linear_second_parameters.csv'))
     linear_expected_fit_adel_input_data_second_dict.update(axis_table=linear_expected_second_axis_table_dataframe, 
                                                     absolute_phen_table=linear_expected_absolute_phen_table_dataframe,
                                                     relative_phen_table=linear_expected_relative_phen_table_dataframe,
                                                     parameters=linear_expected_second_parameters_dataframe)
     
-    bilinear_expected_second_axis_table_dataframe = pandas.read_csv(path('data/bilinear_second_axis_table.csv'))
-    bilinear_expected_absolute_phen_table_dataframe = pandas.read_csv(path('data/bilinear_second_absolute_phen_table.csv'))
-    bilinear_expected_relative_phen_table_dataframe = pandas.read_csv(path('data/bilinear_second_relative_phen_table.csv'))
-    bilinear_expected_second_parameters_dataframe = pandas.read_csv(path('data/bilinear_second_parameters.csv'))
+    bilinear_expected_second_axis_table_dataframe = pandas.read_csv(path('data/test_fitting2/bilinear_second_axis_table.csv'))
+    bilinear_expected_absolute_phen_table_dataframe = pandas.read_csv(path('data/test_fitting2/bilinear_second_absolute_phen_table.csv'))
+    bilinear_expected_relative_phen_table_dataframe = pandas.read_csv(path('data/test_fitting2/bilinear_second_relative_phen_table.csv'))
+    bilinear_expected_second_parameters_dataframe = pandas.read_csv(path('data/test_fitting2/bilinear_second_parameters.csv'))
     bilinear_expected_fit_adel_input_data_second_dict.update(axis_table=bilinear_expected_second_axis_table_dataframe, 
                                                              absolute_phen_table=bilinear_expected_absolute_phen_table_dataframe,
                                                              relative_phen_table=bilinear_expected_relative_phen_table_dataframe,
@@ -66,7 +66,7 @@ def test_fit_adel_input_data_first():
                 
     
 def test_fit_adel_input_data_second_linear(): 
-    user_parameters_dataframe = pandas.read_csv(path('data/linear_user_parameters.csv'))
+    user_parameters_dataframe = pandas.read_csv(path('data/test_fitting2/linear_user_parameters.csv'))
     
     fit_adel_input_data_second_dict = fit_adel_input_data_second(expected_fit_adel_input_data_first_dict['axis_table'], 
                                                                  user_parameters_dataframe)
@@ -82,7 +82,7 @@ def test_fit_adel_input_data_second_linear():
                                                 linear_expected_fit_adel_input_data_second_dict[key].values)
         
 def test_fit_adel_input_data_second_bilinear(): 
-    user_parameters_dataframe = pandas.read_csv(path('data/bilinear_user_parameters.csv'))
+    user_parameters_dataframe = pandas.read_csv(path('data/test_fitting2/bilinear_user_parameters.csv'))
     
     fit_adel_input_data_second_dict = fit_adel_input_data_second(expected_fit_adel_input_data_first_dict['axis_table'], 
                                                                  user_parameters_dataframe)

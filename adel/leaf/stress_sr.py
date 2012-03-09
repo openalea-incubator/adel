cd623dcbe483d5fb85b1ad0f7365aa543cbe601b
Christophe Pradal <christophe.pradal@inria.fr>
2012-03-09 17:41:41 +0000

    new dataflow

--- adel/leaf/stress_sr.py
+++ adel/leaf/stress_sr.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.stats._support import unique
+from alinea.adel.fitting import curvilinear_abscisse
+
 
 class Interval(object):
     def __init__(self, min_, max_):
@@ -35,9 +38,7 @@ class Interval(object):
     def __repr__(self):
         return 'Interval('+str(self.min)+','+str(self.max)+')'
 
-def stress_sr(s, r, d):
-    '''    
-    '''
+def sort_intervals(d):
     intervals = dict(zip( (Interval(m,M) for m, M in d), d.itervalues())) 
 
     keys = list(sorted(intervals))
@@ -50,10 +51,18 @@ def stress_sr(s, r, d):
         assert i < j
         if i.max < j.min:
             intervals[Interval(i.max, j.min)]=1
-
+    j = keys[-1]
+    if j.max < 1.:
+        intervals[Interval(j.max, 1)]=1
 
     keys = list(sorted(intervals))
-    print keys
+    return keys, intervals
+
+def stress_sr(s, r, d):
+    '''    
+    '''
+    keys, intervals = sort_intervals(d)
+    
     i = 0
     interval = keys[i]
     factor = intervals[interval]
@@ -69,11 +78,67 @@ def stress_sr(s, r, d):
             factor = intervals[interval]
         l.append(p_min + (p-interval.min) *factor)
 
-    print s
-    print l
     new_s = np.array(l)
-    p_max = new_s.max()
-
-    new_s = new_s/p_max
 
     return new_s, r,
+
+def stress_xy(s, x, y, d):
+    '''    
+    '''
+    keys, intervals = sort_intervals(d)
+
+    s_new = [keys[0].min]+[k.max for k in keys]
+    s_new = list(sorted(set(s).union(s_new)))
+    x_new = np.interp(s_new, s, x) 
+    y_new = np.interp(s_new, s, y) 
+
+    i = 0
+    interval = keys[i]
+    factor = intervals[interval]
+    p_min = interval.min
+    x_min = x_new[0]
+    y_min = y_new[0]
+
+    dx = np.diff(x_new)
+    dy = np.diff(y_new)
+
+    # p_min
+    l = [p_min]; xl = []; yl=[]
+    for index, p in enumerate(s[1:]):
+        if p not in interval:
+            # accumulate values by taking account of the size of the previous interval modified by the factor
+            p_min += (interval.max-interval.min)*factor
+            i+=1
+            interval = keys[i]
+            factor = intervals[interval]
+
+        l.append(p_min + (p-interval.min) *factor)
+
+        xl.append(dx[index]*factor)
+        yl.append(dy[index]*factor)
+
+    new_x = np.cumsum([x_min]+xl)
+    new_y = np.cumsum([y_min]+yl)
+    new_s = np.array(l)
+
+    return new_s, new_x, new_y
+
+def stress_xysr(x, y, s, r, d):
+    '''    
+    '''
+    keys, intervals = sort_intervals(d)
+
+    new_s, r = stress_sr(s,r,d)
+
+    s_xy = curvilinear_abscisse(x,y)
+    s_max = s_xy.max()
+    s_xy /= s_max
+    new_x = x/s_max
+    new_y = y/s_max
+
+    s_xy, new_x, new_y = stress_xy(s_xy, new_x, new_y,d)
+
+    xy = np.array([x,y])
+    xy = unique(xy.T).T
+    new_x, new_y = xy[0], xy[1]
+    return new_x, new_y, new_s, r 

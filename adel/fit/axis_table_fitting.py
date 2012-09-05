fabfd327c8dbc16fb0fee4001a318b671b874a01
Camille Chambon <cchambon@grignon.inra.fr>
2012-09-05 10:27:00 +0000

    Move axis_table_fitting._find_child_cohort_numbers, axis_table_fitting._create_N_phyt_list and axis_table_fitting._create_TT_stop_axis_list to fitting_tools module.
    Rename axis_table_fitting._create_TT_stop_axis_list to dead_or_alive_decision.

--- adel/fit/axis_table_fitting.py
+++ adel/fit/axis_table_fitting.py
@@ -7,9 +7,10 @@ Created on 28 nov. 2011
 '''
 
 import random
-import math
 import numpy as np
 import pandas
+
+from adel.fit import fitting_tools
 
 # the coefficients of the secondary stem leaves number.
 secondary_stem_leaves_number_coefficients = {'a_1': 0.9423, 'a_2': 0.555}
@@ -45,7 +46,7 @@ def generate_axes(plant_number, cohort_probabilities, main_stem_leaves_number_pr
     plant_ids = range(1,plant_number + 1)
     index_axis_list = _create_index_axis_list(plant_ids, cohort_probabilities)
     index_plt_list = _create_index_plt_list(plant_ids, index_axis_list)
-    N_phyt_list = _create_N_phyt_list(index_axis_list, main_stem_leaves_number_probability_distribution, secondary_stem_leaves_number_coefficients)
+    N_phyt_list = fitting_tools.create_N_phyt_list(index_axis_list, main_stem_leaves_number_probability_distribution, secondary_stem_leaves_number_coefficients)
     TT_stop_axis_list = [np.nan for i in range(len(index_axis_list))]
     TT_del_axis_list = [np.nan for i in range(len(index_axis_list))]
     id_dim_list = _create_id_dim_list(index_axis_list, N_phyt_list)
@@ -85,7 +86,7 @@ def fit_axis_table_second(first_axis_table_dataframe, first_leaf_phen_table_data
      second_axis_table_dataframe['TT_col_phytomer1'], 
      second_axis_table_dataframe['TT_sen_phytomer1'],
      second_axis_table_dataframe['TT_del_phytomer1']) = _create_all_TT_phytomer1_list(first_axis_table_dataframe, emf_1_main_stem_standard_deviation, first_leaf_phen_table_dataframe)
-    second_axis_table_dataframe['TT_stop_axis'] = _create_TT_stop_axis_list(first_axis_table_dataframe.index.size, final_axes_number, second_axis_table_dataframe['TT_em_phytomer1'].tolist(), bolting_date, flowering_date)
+    second_axis_table_dataframe['TT_stop_axis'] = fitting_tools.dead_or_alive_decision(first_axis_table_dataframe.index.size, final_axes_number, second_axis_table_dataframe['TT_em_phytomer1'].tolist(), bolting_date, flowering_date)
     second_axis_table_dataframe['TT_del_axis'] = _create_TT_del_axis_list(second_axis_table_dataframe['TT_stop_axis'], delais_TT_stop_del_axis)
     
     return second_axis_table_dataframe
@@ -133,93 +134,10 @@ def _create_index_axis_list(first_axis_table_plant_ids, cohort_probabilities):
     '''
     index_axis_list = []
     for plant_id in first_axis_table_plant_ids:
-        cohort_numbers = _find_child_cohort_numbers(cohort_probabilities)
+        cohort_numbers = fitting_tools.find_child_cohort_numbers(cohort_probabilities)
         cohort_numbers.sort()
         index_axis_list.extend(cohort_numbers)
     return index_axis_list
-
-
-def _find_child_cohort_numbers(cohort_probabilities, parent_cohort_number=-1, first_child_delay=2):
-    '''
-    Find (recursively) the child cohort numbers of a parent cohort, according to the cohort probabilities and 
-    the parent cohort number. The main stem always exists.
-    :Parameters:
-        - `cohort_probabilities` : the cohort probabilities.
-        - `parent_cohort_number` : the parent cohort number.
-    :Types:
-        - `cohort_probabilities` : dict
-        - `parent_cohort_number` : int
-        
-    :return: The axes column.
-    :rtype: list
-    '''
-    child_cohort_numbers = []
-    first_possible_cohort_number = parent_cohort_number + first_child_delay
-    if first_possible_cohort_number == 1:
-        # The main stem always exists, then add it.
-        child_cohort_numbers.append(first_possible_cohort_number)
-        child_cohort_numbers.extend(_find_child_cohort_numbers(cohort_probabilities, 
-                                                               first_possible_cohort_number))
-    else:
-        # Find the secondary stem children.
-        for cohort_number_str, cohort_probability in cohort_probabilities.iteritems():
-            cohort_number = int(cohort_number_str)
-            if cohort_number >= first_possible_cohort_number:
-                if cohort_probability >= random.random():
-                    child_cohort_numbers.append(cohort_number)
-                    child_cohort_numbers.extend(_find_child_cohort_numbers(cohort_probabilities, 
-                                                                           cohort_number))
-    return child_cohort_numbers
-              
-              
-def _create_N_phyt_list(first_axis_table_index_axis_list, 
-                        main_stem_leaves_number_probability_distribution,
-                        secondary_stem_leaves_number_coefficients):
-    '''
-    Create the nff column.
-    :Parameters:
-        - `first_axis_table_index_axis_list` : the axes column.
-        - `main_stem_leaves_number_probability_distribution` : the probability distribution of 
-        the main stem leaves number.
-        - `secondary_stem_leaves_number_coefficients` : the coefficients of the secondary stem leaves number.
-    :Types:
-        - `first_axis_table_index_axis_list` : list
-        - `main_stem_leaves_number_probability_distribution` : dict
-        - `secondary_stem_leaves_number_coefficients` : dict
-        
-    :return: The nff column.
-    :rtype: list
-    '''
-    N_phyt_list = []
-    main_stem_leaves_number = 0.0
-    # for each plant...
-    for cohort_number in first_axis_table_index_axis_list:
-        # calculate the leaves number of each axis
-        leaves_number_float = 0.0
-        if cohort_number == 1:
-            # It is the main stem, then the leaves number has to satisfy the probability distribution defined  
-            # in main_stem_leaves_number_probability_distribution
-            random_value = random.random()
-            probabilities_sum = 0.0
-            for leaves_number_str, leaves_probability in main_stem_leaves_number_probability_distribution.iteritems():
-                probabilities_sum += leaves_probability
-                if random_value <= probabilities_sum:
-                    main_stem_leaves_number = float(leaves_number_str)
-                    break
-            leaves_number_float = main_stem_leaves_number
-        else:
-            # it is a secondary stem (i.e. a tiller)
-            a_1 = secondary_stem_leaves_number_coefficients['a_1']
-            a_2 = secondary_stem_leaves_number_coefficients['a_2']
-            leaves_number_float = a_1* main_stem_leaves_number - a_2 * cohort_number
-        fractional_part, integer_part = math.modf(leaves_number_float)
-        if random.random() <= fractional_part:
-            leaves_number_int = int(math.ceil(leaves_number_float))
-        else:
-            leaves_number_int = int(integer_part)
-        N_phyt_list.append(leaves_number_int)
-     
-    return N_phyt_list
 
 
 def _create_all_TT_phytomer1_list(first_axis_table_dataframe, emf_1_main_stem_standard_deviation, first_leaf_phen_table_dataframe):
@@ -323,55 +241,6 @@ def _create_id_ear_list(first_axis_table_index_plt_list):
     :rtype: list
     '''
     return ['1' for plant_id in first_axis_table_index_plt_list]
-    
-    
-def _create_TT_stop_axis_list(max_axes_number, min_axes_number, first_axis_table_TT_em_phytomer1, bolting_date, flowering_date):
-    '''
-    Create TT_stop_axis column.
-    :Parameters:
-        - `max_axes_number` : The maximum number of existing axes. Must be positive or null, and greater than min_axes_number.
-        - `min_axes_number` : The minimum number of existing axes. Must be positive or null, and lesser than max_axes_number.
-        - `first_axis_table_TT_em_phytomer1` : The TT_em_phytomer1 column.
-        - `bolting_date` : The bolting date. Must be positive or null, and lesser than flowering_date.
-        - `flowering_date` : The flowering date. Must be positive or null, and greater than bolting_date.
-    :Types:
-        - `max_axes_number` : int
-        - `min_axes_number` : int
-        - `first_axis_table_TT_em_phytomer1` : list
-        - `bolting_date` : int
-        - `flowering_date` : int
-        
-    :return: The TT_stop_axis column.
-    :rtype: pandas.Series
-    '''
-    
-    assert max_axes_number >= 0 and min_axes_number >=0 and bolting_date >= 0 and flowering_date >= 0
-    assert bolting_date <= flowering_date
-    assert min_axes_number <= max_axes_number
-    
-    polynomial_coefficient_array = np.polyfit([flowering_date, bolting_date], [min_axes_number, max_axes_number], 1)
-                
-    remaining_axes_number = max_axes_number
-    T_em_leaf1_tuples = zip(first_axis_table_TT_em_phytomer1[:], range(len(first_axis_table_TT_em_phytomer1)))
-    T_em_leaf1_tuples.sort()
-    T_stop_axis_tuples = []
-    for tt in range(bolting_date, flowering_date + 1):
-        simulated_axes_number = int(np.polyval(polynomial_coefficient_array, tt))
-        axes_to_delete_number = remaining_axes_number - simulated_axes_number
-        while axes_to_delete_number > 0:
-            max_emf_1, axis_row_number = T_em_leaf1_tuples.pop()
-            T_stop_axis_tuples.append((axis_row_number, tt))
-            axes_to_delete_number -= 1
-            remaining_axes_number -= 1
-        if remaining_axes_number == 0:
-            break 
-    T_stop_axis_tuples.sort()
-    T_stop_axis_row_number_list = [T_stop_axis_tuple[0] for T_stop_axis_tuple in T_stop_axis_tuples]
-    TT_stop_axis_list = [T_stop_axis_tuple[1] for T_stop_axis_tuple in T_stop_axis_tuples]
-    for i in range(len(first_axis_table_TT_em_phytomer1)):
-        if i not in T_stop_axis_row_number_list:
-            TT_stop_axis_list.insert(i, np.nan)
-    return TT_stop_axis_list 
     
     
 def _create_TT_del_axis_list(TT_stop_axis_series, delais_TT_stop_del_axis):
---
+++ adel/fit/fitting_tools.py
@@ -0,0 +1,144 @@
+'''
+Created on 4 mai 2012
+
+@author: cchambon
+'''
+
+import random
+import math
+
+import numpy as np
+
+def find_child_cohort_numbers(cohort_probabilities, parent_cohort_number=-1, first_child_delay=2):
+    '''
+    Find (recursively) the child cohort numbers of a parent cohort, according to the cohort probabilities and 
+    the parent cohort number. The main stem always exists.
+    :Parameters:
+        - `cohort_probabilities` : the cohort probabilities.
+        - `parent_cohort_number` : the parent cohort number.
+    :Types:
+        - `cohort_probabilities` : dict
+        - `parent_cohort_number` : int
+        
+    :return: The axes column.
+    :rtype: list
+    '''
+    child_cohort_numbers = []
+    first_possible_cohort_number = parent_cohort_number + first_child_delay
+    if first_possible_cohort_number == 1:
+        # The main stem always exists, then add it.
+        child_cohort_numbers.append(first_possible_cohort_number)
+        child_cohort_numbers.extend(find_child_cohort_numbers(cohort_probabilities, 
+                                                               first_possible_cohort_number))
+    else:
+        # Find the secondary stem children.
+        for cohort_number_str, cohort_probability in cohort_probabilities.iteritems():
+            cohort_number = int(cohort_number_str)
+            if cohort_number >= first_possible_cohort_number:
+                if cohort_probability >= random.random():
+                    child_cohort_numbers.append(cohort_number)
+                    child_cohort_numbers.extend(find_child_cohort_numbers(cohort_probabilities, 
+                                                                           cohort_number))
+    return child_cohort_numbers
+
+
+def create_N_phyt_list(first_axis_table_index_axis_list, 
+                       main_stem_leaves_number_probability_distribution,
+                       secondary_stem_leaves_number_coefficients):
+    '''
+    Create the nff column.
+    :Parameters:
+        - `first_axis_table_index_axis_list` : the axes column.
+        - `main_stem_leaves_number_probability_distribution` : the probability distribution of 
+        the main stem leaves number.
+        - `secondary_stem_leaves_number_coefficients` : the coefficients of the secondary stem leaves number.
+    :Types:
+        - `first_axis_table_index_axis_list` : list
+        - `main_stem_leaves_number_probability_distribution` : dict
+        - `secondary_stem_leaves_number_coefficients` : dict
+        
+    :return: The nff column.
+    :rtype: list
+    '''
+    N_phyt_list = []
+    main_stem_leaves_number = 0.0
+    # for each plant...
+    for cohort_number in first_axis_table_index_axis_list:
+        # calculate the leaves number of each axis
+        leaves_number_float = 0.0
+        if cohort_number == 1:
+            # It is the main stem, then the leaves number has to satisfy the probability distribution defined  
+            # in main_stem_leaves_number_probability_distribution
+            random_value = random.random()
+            probabilities_sum = 0.0
+            for leaves_number_str, leaves_probability in main_stem_leaves_number_probability_distribution.iteritems():
+                probabilities_sum += leaves_probability
+                if random_value <= probabilities_sum:
+                    main_stem_leaves_number = float(leaves_number_str)
+                    break
+            leaves_number_float = main_stem_leaves_number
+        else:
+            # it is a secondary stem (i.e. a tiller)
+            a_1 = secondary_stem_leaves_number_coefficients['a_1']
+            a_2 = secondary_stem_leaves_number_coefficients['a_2']
+            leaves_number_float = a_1* main_stem_leaves_number - a_2 * cohort_number
+        fractional_part, integer_part = math.modf(leaves_number_float)
+        if random.random() <= fractional_part:
+            leaves_number_int = int(math.ceil(leaves_number_float))
+        else:
+            leaves_number_int = int(integer_part)
+        N_phyt_list.append(leaves_number_int)
+     
+    return N_phyt_list
+
+
+def dead_or_alive_decision(max_axes_number, min_axes_number, first_axis_table_TT_em_phytomer1, bolting_date, flowering_date):
+    '''
+    Algorithme pour la decision de l'arret de croissance d'une talle en fonction d'une loi de decroissance d'effectif 
+    sur la population globale.
+    (previous name: Create TT_stop_axis column).
+    :Parameters:
+        - `max_axes_number` : The maximum number of existing axes. Must be positive or null, and greater than min_axes_number.
+        - `min_axes_number` : The minimum number of existing axes. Must be positive or null, and lesser than max_axes_number.
+        - `first_axis_table_TT_em_phytomer1` : The TT_em_phytomer1 column.
+        - `bolting_date` : The bolting date. Must be positive or null, and lesser than flowering_date.
+        - `flowering_date` : The flowering date. Must be positive or null, and greater than bolting_date.
+    :Types:
+        - `max_axes_number` : int
+        - `min_axes_number` : int
+        - `first_axis_table_TT_em_phytomer1` : list
+        - `bolting_date` : int
+        - `flowering_date` : int
+        
+    :return: The TT_stop_axis column.
+    :rtype: pandas.Series
+    '''
+    
+    assert max_axes_number >= 0 and min_axes_number >=0 and bolting_date >= 0 and flowering_date >= 0
+    assert bolting_date <= flowering_date
+    assert min_axes_number <= max_axes_number
+    
+    polynomial_coefficient_array = np.polyfit([flowering_date, bolting_date], [min_axes_number, max_axes_number], 1)
+                
+    remaining_axes_number = max_axes_number
+    T_em_leaf1_tuples = zip(first_axis_table_TT_em_phytomer1[:], range(len(first_axis_table_TT_em_phytomer1)))
+    T_em_leaf1_tuples.sort()
+    T_stop_axis_tuples = []
+    for tt in range(bolting_date, flowering_date + 1):
+        simulated_axes_number = int(np.polyval(polynomial_coefficient_array, tt))
+        axes_to_delete_number = remaining_axes_number - simulated_axes_number
+        while axes_to_delete_number > 0:
+            max_emf_1, axis_row_number = T_em_leaf1_tuples.pop()
+            T_stop_axis_tuples.append((axis_row_number, tt))
+            axes_to_delete_number -= 1
+            remaining_axes_number -= 1
+        if remaining_axes_number == 0:
+            break 
+    T_stop_axis_tuples.sort()
+    T_stop_axis_row_number_list = [T_stop_axis_tuple[0] for T_stop_axis_tuple in T_stop_axis_tuples]
+    TT_stop_axis_list = [T_stop_axis_tuple[1] for T_stop_axis_tuple in T_stop_axis_tuples]
+    for i in range(len(first_axis_table_TT_em_phytomer1)):
+        if i not in T_stop_axis_row_number_list:
+            TT_stop_axis_list.insert(i, np.nan)
+    return TT_stop_axis_list 
+

7423c6655b863122f1c9eeb34d7f26e3e7d9e85d
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2012-10-01 16:22:14 +0000

    minor update of newmtg test

--- test/test_newmtg.py
+++ test/test_newmtg.py
@@ -11,5 +11,5 @@ def display(g,scale=-1):
 #affichage : aller voir __str__dans mtg.py
 dp = {'plant':[1,1,1,1,2], 'axe' : [0,0,1,1,0], 'numphy': [1,2,1,2,1]}
 
-g=mtg_factory(dp,wheat_metamer)
+g=mtg_factory(dp,adel_metamer)
 print g

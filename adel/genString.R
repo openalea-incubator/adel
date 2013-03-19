d186318b1eff26c69a4beb62e2bad314e748332a
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2013-03-19 23:38:18 +0000

    update genstring to account for new id_axis coding and new visibility computations

--- adel/genString.R
+++ adel/genString.R
@@ -34,68 +34,73 @@ pitch <- function(inc) paste("+(",round(inc,2),")",sep="")
 roll <- function(ang) paste("/(",round(ang,2),")",sep="")
 up <- function(ang) paste("^(",round(ang,2),")",sep="")
 #
-Internode <- function(lgreen,lsen,diam,po,pos,epsillon) {
+Internode <- function(lv,lvsen,diam,po,pos,epsillon) {
   chn <- ""
-#  if (lgreen > epsillon) # no condition, as internode should always be there to allow bearing tillers
-    chn <- paste(chn,StemElement(ttype$green_internode,lgreen,diam,diam),sep="")
-  #if (lsen > epsillon)
-    chn <- paste(chn,StemElement(ttype$sen_internode,lsen,diam,diam))
+  lvgreen = lv - lvsen
+#  if (lvgreen > epsillon) # no condition, as internode should always be there to allow bearing tillers
+    chn <- paste(chn,StemElement(ttype$green_internode,lvgreen,diam,diam),sep="")
+  #if (lvsen > epsillon)
+    chn <- paste(chn,StemElement(ttype$sen_internode,lvsen,diam,diam))
   chn
 }
 #
-Peduncle <- function(lgreen,lsen,diam,po,pos,epsillon) {
+Peduncle <- function(lv,lvsen,diam,po,pos,epsillon) {
   chn <- ""
-  if (lgreen > epsillon)
-    chn <- paste(chn,StemElement(ttype$green_peduncle,lgreen,diam,diam),sep="")
-  if (lsen > epsillon)
-    chn <- paste(chn,StemElement(ttype$sen_peduncle,lsen,diam,diam))
+  lvgreen = lv - lvsen
+  if (lvgreen > epsillon)
+    chn <- paste(chn,StemElement(ttype$green_peduncle,lvgreen,diam,diam),sep="")
+  if (lvsen > epsillon)
+    chn <- paste(chn,StemElement(ttype$sen_peduncle,lvsen,diam,diam))
   chn
 }
 #
-Ear <- function(lgreen,lsen,diam,po,pos,epsillon) {
+Ear <- function(lv,lvsen,diam,po,pos,epsillon) {
   chn <- ""
-  if (lgreen > epsillon)
-    chn <- paste(chn,StemElement(ttype$green_ear,lgreen,diam,diam),sep="")
-  if (lsen > epsillon)
-    chn <- paste(chn,StemElement(ttype$sen_ear,lsen,diam,diam))
+  lvgreen = lv - lvsen
+  if (lvgreen > epsillon)
+    chn <- paste(chn,StemElement(ttype$green_ear,lvgreen,diam,diam),sep="")
+  if (lvsen > epsillon)
+    chn <- paste(chn,StemElement(ttype$sen_ear,lvsen,diam,diam))
   chn
 }
 #
-Awn <- function(lgreen,lsen,diam,po,pos,epsillon) {
+Awn <- function(lv,lvsen,diam,po,pos,epsillon) {
   chn <- ""
-  if (lgreen > epsillon)
-    chn <- paste(chn,StemElement(ttype$green_awn,lgreen,diam,diam),sep="")
-  if (lsen > epsillon)
-    chn <- paste(chn,StemElement(ttype$sen_awn,lsen,diam,diam))
+  lvgreen = lv - lvsen
+  if (lvgreen > epsillon)
+    chn <- paste(chn,StemElement(ttype$green_awn,lvgreen,diam,diam),sep="")
+  if (lvsen > epsillon)
+    chn <- paste(chn,StemElement(ttype$sen_awn,lvsen,diam,diam))
   chn
 }
 #
-Sheath <- function(lgreen,lsen,diam,po,pos,epsillon) {
+Sheath <- function(lv,lvsen,diam,po,pos,epsillon) {
   chn <- ""
-  if (lgreen > epsillon)
-    chn <- paste(chn,StemElement(ttype$green_sheath,lgreen,diam,diam),sep="")
-  if (lsen > epsillon)
-    chn <- paste(chn,StemElement(ttype$sen_sheath,lsen,diam,diam))
+  lvgreen = lv - lvsen
+  if (lvgreen > epsillon)
+    chn <- paste(chn,StemElement(ttype$green_sheath,lvgreen,diam,diam),sep="")
+  if (lvsen > epsillon)
+    chn <- paste(chn,StemElement(ttype$sen_sheath,lvsen,diam,diam))
   chn
 }
 #
-Blade <- function(lv, lsen, Lf, wM, lsenshrink,lctype, lcindex, incB, po, pos, epsillon,randindex) {
+Blade <- function(lv, lvsen, Lf, wM, lsenshrink,lctype, lcindex, incB, po, pos, epsillon,randindex) {
   chn <- ""
-  lgreen = lv - lsen
+  lvgreen = lv - lvsen
   if (randindex) {#lcindex has not been set by adel)
-    if (lgreen > epsillon)
+    if (lvgreen > epsillon)
       chn <- paste(chn,
-                   LeafElement(ttype$green_lamina, Lf, lv, wM, 0, lgreen / lv,lctype, lcindex, incB))
-    if (lsen > epsillon)
+                   LeafElement(ttype$green_lamina, Lf, lv, wM, 0, lvgreen / lv,lctype, lcindex, incB))
+    if (lvsen > epsillon)
       chn <- paste(chn,
-                   LeafElement(ttype$sen_lamina, Lf, lv, wM * lsenshrink, lgreen / lv, 1, lctype, lcindex, incB))
+                   LeafElement(ttype$sen_lamina, Lf, lv, wM * lsenshrink, lvgreen / lv, 1, lctype, lcindex, incB))
   } else {
-    if (lgreen > epsillon)
+    if (lvgreen > epsillon)
       chn <- paste(chn,
-                   indexedLeafElement(ttype$green_lamina, Lf, lv, wM, 0, lgreen / lv,lctype, lcindex, incB,lcindex))
-    if (lsen > epsillon)
+                   indexedLeafElement(ttype$green_lamina, Lf, lv, wM, 0, lvgreen / lv,lctype, lcindex, incB,lcindex))
+    if (lvsen > epsillon)
       chn <- paste(chn,
-                   indexedLeafElement(ttype$sen_lamina, Lf, lv, wM * lsenshrink, lgreen / lv, 1, lctype, lcindex, incB,lcindex))
+                   indexedLeafElement(ttype$sen_lamina, Lf, lv, wM * lsenshrink, lvgreen / lv, 1, lctype, lcindex, incB,lcindex))
   }
  
   chn
@@ -112,7 +117,7 @@ Metamer <- function(dat,epsillon,azcum,axil = NULL) {
 
   #if (dat$Ev > epsillon) #not tested to ensure persistence of stems even with 0 length (for tiller attachment)
   chn <- paste(chn,
-                 Internode(dat$Ev-dat$Esen,dat$Esen,dat$Ed,dat$Epo,dat$Epos,epsillon))
+                 Internode(dat$Ev,min(dat$Esen,dat$Ev),dat$Ed,dat$Epo,dat$Epos,epsillon))
 
   if (abs(dat$Ginc) > 0)
     chn <- paste(chn,
@@ -120,13 +125,13 @@ Metamer <- function(dat,epsillon,azcum,axil = NULL) {
 
   if (dat$Gv > epsillon)
     chn <- paste(chn,
-                 Sheath(dat$Gv-dat$Gsen,dat$Gsen,dat$Gd,dat$Gpo,dat$Gpos,epsillon))
+                 Sheath(dat$Gv,min(dat$Gsen,dat$Gv),dat$Gd,dat$Gpo,dat$Gpos,epsillon))
 
   if (dat$Lv > epsillon) 
     chn <- paste(chn,
                  "[",
                  roll(azm),
-                 Blade(dat$Lv, dat$Lsen, dat$Ll,dat$Lw,dat$LsenShrink,dat$LcType,dat$LcIndex,dat$Linc,dat$Lpo,dat$Lpos,epsillon,all(dat$LcIndex <= 1)),
+                 Blade(dat$Lv, min(dat$Lsen,dat$Lv), dat$Ll,dat$Lw,dat$LsenShrink,dat$LcType,dat$LcIndex,dat$Linc,dat$Lpo,dat$Lpos,epsillon,all(dat$LcIndex <= 1)),
                  "]")
   
   if (!is.null(axil))
@@ -139,24 +144,24 @@ Metamer <- function(dat,epsillon,azcum,axil = NULL) {
                  "[",
                  roll(azm),
                  "newAxe")
-      nf <- max(1,nrow(ax) - 3)
+      nf <- nrow(ax) - ifelse(nrow(ax) > 3, 3,0)
       for (n in ax$numphy[1:nf]) {
         chn <- paste(chn,Metamer(ax[ax$numphy == n,],epsillon,azaxil))
         azaxil = azaxil + ax$Laz[ax$numphy == n]
       }
-	  if (nf > 3) {
+	  if (nrow(ax) > 3) {
                                         #add Peduncle
       if (ax$Ev[nf + 1] > epsillon)
         chn <- paste(chn,
-                 Peduncle(ax$Ev[nf + 1]-ax$Esen[nf + 1],ax$Esen[nf + 1],ax$Ed[nf + 1],ax$Epo[nf + 1],ax$Epos[nf + 1],epsillon))
+                 Peduncle(ax$Ev[nf + 1],min(ax$Esen[nf + 1],ax$Ev[nf + 1]),ax$Ed[nf + 1],ax$Epo[nf + 1],ax$Epos[nf + 1],epsillon))
                                         #add ear
       if (ax$Ev[nf + 2] > epsillon)
         chn <- paste(chn,
-                 Ear(ax$Ev[nf + 2]-ax$Esen[nf + 2],ax$Esen[nf + 2],ax$Ed[nf + 2],ax$Epo[nf + 2],ax$Epos[nf + 2],epsillon))
+                 Ear(ax$Ev[nf + 2],min(ax$Esen[nf + 2],ax$Ev[nf + 2]),ax$Ed[nf + 2],ax$Epo[nf + 2],ax$Epos[nf + 2],epsillon))
     #add awn
       if (ax$Ev[nf + 3] > epsillon)
         chn <- paste(chn,
-                 Awn(ax$Ev[nf + 3]-ax$Esen[nf + 3],ax$Esen[nf + 3],ax$Ed[nf + 3],ax$Epo[nf + 3],ax$Epos[nf + 3],epsillon))
+                 Awn(ax$Ev[nf + 3],min(ax$Esen[nf + 3],ax$Ev[nf + 3]),ax$Ed[nf + 3],ax$Epo[nf + 3],ax$Epos[nf + 3],epsillon))
       }
 	  chn <- paste(chn,
                  "]")
@@ -164,20 +169,33 @@ Metamer <- function(dat,epsillon,azcum,axil = NULL) {
   chn
 }
 #
+#axe_code = 0 pour MS, num bearing metamer pour Tiller
+#
+axe_code <- function(axeid) {
+  idcode <- strsplit(axeid,split=".",fixed=TRUE)[[1]][1]
+  if (idcode=="MS")
+    code <- 0
+  else
+    code <- max(1,as.numeric(strsplit(idcode,split="T")[[1]][2])) # T0 attached to phyto1
+  code
+}
+
+#
 genString <- function(can,pars=list("epsillon" = 1e-6)) {
   epsillon = pars$epsillon
   chn <- ""
   if (!is.null(can)) {#no plant is emerged
     for (p in unique(can$plant)) {
       pl <- can[can$plant == p,]
+      pl$axe <- axe_code(as.character(pl$axe_id))
       axes <- unique(pl$axe)
-      axe <- pl[pl$axe == 0,]#main stem
+      axe <- pl[pl$axe_id == 'MS',]#main stem
       if (nrow(axe) > 0) {
         azcum <- 0
         chn <- paste(chn,
                  "[ newPlant newAxe")
                                         # stringify main stem and delegates to Metamer axilary branches, if any
-        nf <- max(nrow(axe) - 3,1)
+        nf <- nrow(axe) - ifelse(nrow(axe) > 3, 3, 0)
         for (m in axe$numphy[1:nf]) {
           if (m %in% axes)
             chn <- paste(chn,Metamer(axe[axe$numphy == m,],epsillon,azcum,pl[pl$axe == m,]))
@@ -185,19 +203,19 @@ genString <- function(can,pars=list("epsillon" = 1e-6)) {
             chn <- paste(chn,Metamer(axe[axe$numphy == m,],epsillon,azcum))
           azcum <- azcum + axe$Laz[axe$numphy == m]
         }
-		if (nf > 3) {
+		if (nrow(axe) > 3) {
                                         #add Peduncle
         if (axe$Ev[nf + 1] > epsillon)
           chn <- paste(chn,
-                 Peduncle(axe$Ev[nf + 1]-axe$Esen[nf + 1],axe$Esen[nf + 1],axe$Ed[nf + 1],axe$Epo[nf + 1],axe$Epos[nf + 1],epsillon))
+                 Peduncle(axe$Ev[nf + 1],min(axe$Esen[nf + 1],axe$Ev[nf + 1]),axe$Ed[nf + 1],axe$Epo[nf + 1],axe$Epos[nf + 1],epsillon))
                                         #add ear
         if (axe$Ev[nf + 2] > epsillon)
           chn <- paste(chn,
-                 Ear(axe$Ev[nf + 2]-axe$Esen[nf + 2],axe$Esen[nf + 2],axe$Ed[nf + 2],axe$Epo[nf + 2],axe$Epos[nf + 2],epsillon))
+                 Ear(axe$Ev[nf + 2],min(axe$Esen[nf + 2],axe$Ev[nf + 2]),axe$Ed[nf + 2],axe$Epo[nf + 2],axe$Epos[nf + 2],epsillon))
                                         #add awn
         if (axe$Ev[nf + 3] > epsillon)
           chn <- paste(chn,
-                 Awn(axe$Ev[nf + 3]-axe$Esen[nf + 3],axe$Esen[nf + 3],axe$Ed[nf + 3],axe$Epo[nf + 3],axe$Epos[nf + 3],epsillon))
+                 Awn(axe$Ev[nf + 3],min(axe$Esen[nf + 3],axe$Ev[nf + 3]),axe$Ed[nf + 3],axe$Epo[nf + 3],axe$Epos[nf + 3],epsillon))
         }
 		chn <- paste(chn,"]")
       }

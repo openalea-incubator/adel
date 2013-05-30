ce09761406c52f31a3f863501a0015387d578ec5
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2013-05-30 21:10:43 +0000

    repair test_adelR.py

--- example/test_adelR.py
+++ example/test_adelR.py
@@ -12,7 +12,7 @@ def leaves_db():
     leaves = Pickle.load(f)
     f.close()
     leaves = fit_leaves(leaves, 9)
-    return leaves
+    return leaves[0]
 
 
 leafdb = leaves_db()
@@ -48,12 +48,12 @@ def newmtg(canopy,dec=10,az=0):
     return g,s
 
 def test_positioning():
-    d = {'plant':[1,1,1],'axe':[0,1,1],'numphy':[1,1,2], 'axe_id':[0,1,1],
+    d = {'plant':[1,1,1],'axe_id':['MS','T1','T1'],'ms_insertion':[0,1,1],'numphy':[1,1,2],
          'Laz': [0,90,180], 'Ll' :[3,3,3], 'Lv' :[3,3,3] , 'Lsen':[0,0,1], 'L_shape':[3,3,3], 'Lw_shape':[.3,.3,.3], 'Linc':[0,0,0],
          'Einc':[0,45,-45],'El':[1,1,1],'Ev':[1,1,1],'Esen':[0,0,0],'Ed': [0.1,0.1,0.1], 
          'Ginc':[0,0,0],'Gl':[0,0,0],'Gv':[0,0,0],'Gsen':[0,0,0],'Gd': [0.1,0.1,0.1], 
          'Epo':[1,1,1], 'Epos':[1,1,1], 'Gpo':[1,1,1], 'Gpos':[1,1,1], 'Lpo':[1,1,1], 'Lpos':[1,1,1],
-         'LsenShrink':[1,1,1], 'LcType':[1,1,1], 'LcIndex':[1,1,1]}
+         'LsenShrink':[1,1,1], 'LcType':[1,1,1], 'LcIndex':[1,1,1], 'Lr':[0,0,0]}
     chn = getString(d)
     g1,s1 = canMTG(chn)
     g2,s2 = newmtg(d,dec=5)

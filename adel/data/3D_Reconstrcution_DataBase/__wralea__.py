8ba42448542f0b9670ce05805f8cc25785fc4e6b
Mariem Abichou <mariem.abichou@grignon.inra.fr>
2013-08-13 13:01:56 +0000

    ADD EGC DATA BASE (Maxwell_2011_d220_N1 ; Caphorn_2005_d250_N0; Caphorn_2005_d250_N1 ; Soissons_2005_d250_N0 ; Soissons_2005_d250_N1 ; Soissons_2009_d77_N1 ;Soissons_2009_d228_N1 ; Soissons_2009_d514_N1)

---
+++ adel/data/3D_Reconstrcution_DataBase/__wralea__.py
@@ -0,0 +1,261 @@
+
+# This file has been generated at Mon Aug 12 17:58:12 2013
+
+from openalea.core import *
+
+
+__name__ = 'alinea.adel.data.EGC_dataBase'
+
+__editable__ = True
+__description__ = ''
+__license__ = ''
+__url__ = ''
+__alias__ = ['3D_Reconstrcution_DataBase', 'adel.data.EGC_dataBase']
+__version__ = ''
+__authors__ = ''
+__institutes__ = ''
+__icon__ = 'icon.png'
+
+
+__all__ = ['_150711056', '_274141264', '_274141360', '_150708336', '_150709424', '_150711632', '_274141520', '_150712304', '_150712048', '_150709136', '_274141392', '_274141296', '_150711440', '_150712080', '_274141424', '_150708784', '_274141232', '_274141488', '_274141552', '_150711920', '_150711024', '_274141328', '_150712176', '_150709296', '_150709392', '_150712208', '_150712112', '_150712240', '_150711376', '_150711536', '_150711472', '_150710992', '_150711600', '_274141456']
+
+
+_150711056 = DataFactory(name='Soissons_2005_d250_N0_earT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141264 = DataFactory(name='Caphorn_2005_d250_N0_dimT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141360 = DataFactory(name='Caphorn_2005_d250_N1_axeT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150708336 = DataFactory(name='Caphorn_2005_d250_N1_laminaCur_7c.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150709424 = DataFactory(name='Soissons_2005_d250_N1_earT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150711632 = DataFactory(name='Soissons_2005_d250_N0_laminaCur_7c.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141520 = DataFactory(name='Soissons_2005_d250_N1_lamina2D_7c.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150712304 = DataFactory(name='Soissons_2005_d250_N0_lamina2D_7c.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150712048 = DataFactory(name='Caphorn_2005_d250_N0_lamina2D_7c.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150709136 = DataFactory(name='Caphorn_2005_d250_N1_earT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141392 = DataFactory(name='Caphorn_2005_d250_N1_dimT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141296 = DataFactory(name='Caphorn_2005_d250_N0_laminaCur_7c.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150711440 = DataFactory(name='Caphorn_2005_d250_N1_GroudCover_mesure.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150712080 = DataFactory(name='Caphorn_2005_d250_N0_axeT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141424 = DataFactory(name='Soissons_2005_d250_N1_axeT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150708784 = DataFactory(name='Caphorn_2005_d250_N1_phenT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141232 = DataFactory(name='Soissons_2005_d250_N0_dimT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141488 = DataFactory(name='Caphorn_2005_d250_N0_GroudCover_mesure.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141552 = DataFactory(name='Soissons_2005_d250_N0_GroundCover_mesure.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150711920 = DataFactory(name='Maxwell_2011_d220_N1_dimT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150711024 = DataFactory(name='Maxwell_2011_d220_N1_phenT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141328 = DataFactory(name='Maxwell_2011_d220_N1_VariablesMesurees.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150712176 = DataFactory(name='Caphorn_2005_d250_N0_earT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150709296 = DataFactory(name='Soissons_2005_d250_N1_phenT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150709392 = DataFactory(name='Maxwell_2011_d220_N1_axeT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150712208 = DataFactory(name='Soissons_2005_d250_N0_axeT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150712112 = DataFactory(name='Soissons_2005_d250_N0_phenT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150712240 = DataFactory(name='Info_General_Experimentations.txt',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150711376 = DataFactory(name='Caphorn_2005_d250_N0_phenT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150711536 = DataFactory(name='Soissons_2005_d250_N1_laminaCur_7c.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150711472 = DataFactory(name='ssi2sen.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150710992 = DataFactory(name='Caphorn_2005_d250_N1_lamina2D_7c.RData',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_150711600 = DataFactory(name='Soissons_2005_d250_N1_dimT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_274141456 = DataFactory(name='Maxwell_2011_d220_N1_earT.csv',
+                    description='',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+

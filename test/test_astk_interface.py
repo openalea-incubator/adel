41ae59f50c2be99e72fbe959a708be1024e10b0d
Camille Chambon <cchambon@grignon.inra.fr>
2014-01-16 15:37:57 +0000

    Complete test_postprocessing(...) to test the postprocessing.

--- test/test_astk_interface.py
+++ test/test_astk_interface.py
@@ -1,7 +1,9 @@
 from alinea.adel.astk_interface import AdelWheat
 from alinea.astk.TimeControl import TimeControlSet
+from alinea.adel import postprocessing
 
-wheat = AdelWheat()
+nplants=1
+wheat = AdelWheat(nplants=nplants)
 
 def test_usage():
     g = wheat.setup_canopy()
@@ -11,7 +13,11 @@ def test_usage():
         wheat.grow(g,tc)
     return g
     
-def test_postprocessing(age=10, convert=False):
+def test_postprocessing(age=100):
     g = wheat.setup_canopy(age=age)
-    out = wheat.get_exposed_areas(g,convert=convert)
-    return g, out
+    out = wheat.get_exposed_areas(g,convert=True)
+    domain_area = 1
+    axis_df = postprocessing.axis_statistics(out, domain_area)
+    plot_df = postprocessing.plot_statistics(axis_df, nplants, domain_area)
+    return axis_df, plot_df 
+

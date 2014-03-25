4aeb05e0df9e088c40e82ea219aadc0392e805a0
Camille Chambon <cchambon@grignon.inra.fr>
2014-03-25 10:23:00 +0000

    Externalize "tissue_type" (property_name) and "colors".

--- adel/macro/calculateTCViaPovray.py
+++ adel/macro/calculateTCViaPovray.py
@@ -1,7 +1,10 @@
 def calculateTCViaPovray(plantgl_scene, output_directory='', thermal_time=0, camera_distance=200.0, 
                          fov=50.0, width=4288, height=2848, domain=((0, 0), (1, 1)), 
                          azimuth=0.0, zenith=0.0, camera_type='perspective', soil=False, 
-                         command='povray'):
+                         command='povray', colors=[[0, 255, 0], [255, 0, 0], [0, 255, 0], 
+                                                   [255, 0, 0], [0, 255, 0], [255, 0, 0], 
+                                                   [0, 255, 0], [255, 0, 0], [0, 255, 0], 
+                                                   [255, 0, 0], [0, 255, 0], [255, 0, 0]]):
     # genFilepath
     import os
     pov_file = os.path.join(output_directory, '%s%s%s' % ('scene_', thermal_time, '.pov'))
@@ -9,7 +12,6 @@ def calculateTCViaPovray(plantgl_scene, output_directory='', thermal_time=0, cam
     from alinea.adel.povray import povray
     povray_image, stand_box_image = povray.povray(plantgl_scene, pov_file, camera_distance, fov, width, height, domain, azimuth, zenith, camera_type, soil, command)
     # count_pixels
-    colors = [[0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0]]
     count_pixels_result = os.path.join(output_directory, '%s%s' % ('_outputpovray_', '.csv'))
     from alinea.adel.povray import post_processing
     count_pixels_result = post_processing.count_pixels(povray_image, stand_box_image, colors, count_pixels_result)
--- adel/macro/visualization3D.py
+++ adel/macro/visualization3D.py
@@ -1,12 +1,13 @@
-def visualization3D(stand_MTG):
-    # colors
-    colors = [[0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0]]
+def visualization3D(stand_MTG, property_name='tissue_type', 
+                    colors=[[0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], 
+                            [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0], 
+                            [0, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 0]]):
     # col_item
     from alinea.adel.povray import povray
     lambda_function = povray.col_item(None, colors)
     # apply_property
     from alinea.adel import mtg
-    new_values = mtg.apply_property(stand_MTG, 'tissue_type', lambda_function)
+    new_values = mtg.apply_property(stand_MTG, property_name, lambda_function)
     # to_plantgl
     plantgl_scene = mtg.to_plantgl(stand_MTG, (0,180,0), (0, 130, 0), (170,85,0), new_values, True)[0]
     # plot3D

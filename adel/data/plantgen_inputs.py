1e0133b178f61f12755578dcc18764fa266923ca
Mariem Abichou <mariem.abichou@grignon.inra.fr>
2015-04-22 14:22:28 +0000

    modif input data

--- adel/data/plantgen_inputs.py
+++ adel/data/plantgen_inputs.py
@@ -1,21 +1,14 @@
 # This file stores a set of data which can be used by the function 
 # alinea.adel.plantgen.read_plantgen_inputs to define the inputs of the function 
 # alinea.adel.plantgen.gen_adel_input_data.
-plants_number = 203
-plants_density = 203
-decide_child_axis_probabilities = {'T0': 0.0, 'T1': 0.900,
-                                   'T2': 0.983, 'T3': 0.817,
-                                   'T4': 0.117}
-MS_leaves_number_probabilities = {'10': 0.145,
-                                  '11': 0.818,
-                                  '12': 0.037,
-                                  '13': 0.0,
-                                  '14': 0.0}
-TT_bolting = 980
-final_axes_density = 444
-ears_density = 491
-GL_number = {1732.1: 4.03631578947369, 1818.05:3.01047368421053,
-             1959.55:1.90263157894737, 2108.65:0.0}
+plants_number = 220
+plants_density = 217
+decide_child_axis_probabilities = {'T0': 0.0, 'T1': 0.883,'T2': 0.966, 'T3': 0.817,'T4': 0.083}
+MS_leaves_number_probabilities = {'10': 0.145, '11': 0.818,  '12': 0.037, '13': 0.0, '14': 0.0}
+TT_bolting = 584
+
+ears_density = 344
+GL_number = {1117.0: 5.567, 1212.1:5.312,  1368.7:4.750, 1686.8:2.524, 1818.0:0.0}
 delais_TT_stop_del_axis = 600
 TT_hs_break = 0.0
 inner_params = {'DELAIS_REG_MONT': -40.0, 'TT_DEL_FHAUT': 3000}

0ceacec44560201b655d003ff3b04e77e5b8e672
Camille Chambon <cchambon@grignon.inra.fr>
2015-08-19 09:52:27 +0000

    Debug the tutorials of PlantGen.

--- adel/data/plantgen_inputs.py
+++ adel/data/plantgen_inputs.py
@@ -3,12 +3,10 @@
 # alinea.adel.plantgen.gen_adel_input_data.
 plants_number = 220
 plants_density = 217
-decide_child_axis_probabilities = {'T0': 0.0, 'T1': 0.883,'T2': 0.966, 'T3': 0.817,'T4': 0.083}
+decide_child_axis_probabilities = {'T0': 0.0, 'T1': 0.883,'T2': 0.967, 'T3': 0.817,'T4': 0.083}
 MS_leaves_number_probabilities = {'10': 0.145, '11': 0.818,  '12': 0.037, '13': 0.0, '14': 0.0}
 TT_bolting = 584
-
 ears_density = 344
-GL_number = {1117.0: 5.567, 1212.1:5.312,  1368.7:4.750, 1686.8:2.524, 1818.0:0.0}
+GL_number = {1117.0: 5.567, 1212.1:5.312, 1368.7:4.750, 1686.8:2.524, 1818.0:0.0}
 delais_TT_stop_del_axis = 600
-TT_hs_break = None
 inner_params = {'DELAIS_REG_MONT': -40.0, 'TT_DEL_FHAUT': 3000}

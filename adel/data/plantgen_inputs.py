bba30fa185702fcc6d81305c0040189a9ae0ada1
Camille Chambon <cchambon@grignon.inra.fr>
2015-11-25 14:08:59 +0000

    Remove the parameter TT_bolting because it is not used anymore.

--- adel/data/plantgen_inputs.py
+++ adel/data/plantgen_inputs.py
@@ -5,7 +5,6 @@ plants_number = 220
 plants_density = 217
 decide_child_axis_probabilities = {'T0': 0.0, 'T1': 0.883,'T2': 0.967, 'T3': 0.817,'T4': 0.083}
 MS_leaves_number_probabilities = {'10': 0.145, '11': 0.818,  '12': 0.037, '13': 0.0, '14': 0.0}
-TT_bolting = 584
 ears_density = 344
 GL_number = {1117.0: 5.567, 1212.1:5.312, 1368.7:4.750, 1686.8:2.524, 1818.0:0.0}
 delais_TT_stop_del_axis = 600

d33049861afa3c5c1e6adb562c1e97581f5ed5a1
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2017-06-20 22:35:52 +0200

    add file

---
+++ src/alinea/adel/wheat/Write_Table.py
@@ -0,0 +1,20 @@
+def Write_Table(Table,filename,first=True,sep=' '):
+    '''
+    returns a string representation of the content of the table
+    '''
+    
+    header = [sep.join(Table.keys())]
+    rows = zip(*[Table[k] for k in Table.keys()])
+    lines = [sep.join(map(str,r)) for r in rows]
+    mode = "a"
+    if first:
+        mode = "w"
+    fout = open(filename,mode)
+    if first :
+        fout.write('\n'.join(header + lines))
+    else :
+        fout.write('\n')
+        fout.write('\n'.join(lines))
+    fout.close()
+    
+    return filename,

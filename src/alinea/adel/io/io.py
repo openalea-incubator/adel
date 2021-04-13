f170fd750d238592a0651ef93bfb2e8fbea66c73
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2021-04-13 16:53:44 +0200

    restore import used in wralea

--- src/alinea/adel/io/io.py
+++ src/alinea/adel/io/io.py
@@ -6,6 +6,13 @@ import numpy
 from rpy2 import robjects
 r = robjects.r
 
+# needed for wralea
+from alinea.adel.AdelR import RlistAsDict,readRData,saveRData,csvAsDict,dataframeAsdict,canL2canS
+from alinea.adel.mtg import (mtg_factory, duplicate, thermal_time,
+                             apply_property, to_plantgl, to_canestra)
+from openalea.mtg.io import lpy2mtg, mtg2lpy
+
+import openalea.core.path as path
 
 def _is_iterable(x):
     try:
